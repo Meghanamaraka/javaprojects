31f5fe6086c5517f9eeccb3ac78535aa1894bb5f
Meghanamaraka <marakameghana032@gmail.com>
2025-06-04 16:11:13 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,23 @@
+package xyz;
+
+import java.util.Scanner;
+
+public class EvenorOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else if (num % 2 != 0) {
+            System.out.println(num + " is Odd");
+        } else {
+            System.out.println("Invalid input");
+        }
+
+        scanner.close();
+    }
+}
+
